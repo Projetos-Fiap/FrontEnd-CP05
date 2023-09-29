f74cb328bcb019e16cad7c4180c5df5e884e8633
LucasFeijoDev <LucasFeijoDev@gmail.com>
2023-09-28 21:44:14 -0300

    'Inicial'

---
+++ src/Feminino.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Feminino = () => {
+  return (
+    <div>
+      <h1>Produtos femininos</h1>
+    </div>
+  )
+}
+
+export default Feminino
---
+++ src/Masculino.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Masculino = () => {
+  return (
+    <div>
+      <h1>Produtos masculinos</h1>
+    </div>
+  )
+}
+
+export default Masculino
