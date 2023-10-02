01b9035577c19e60b98d5ca99da5bf66af0fca8e
andrelambert <rm99148@fiap.com.br>
2023-10-02 20:46:34 -0300

    criando carrossel nas paginas, e update readme

--- src/Casual.jsx
+++ src/Casual.jsx
@@ -1,74 +1,60 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import ListGroup from "react-bootstrap/ListGroup";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import "./css/casual.css";
-function Casual() {
+import React from 'react'
+import styled from 'styled-components';
+
+// Importando carrossel do bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import Carousel from "react-bootstrap/Carousel";
+
+// Importando imagens
+import img1 from './assets/casual/casual-1.jpg'
+import img2 from './assets/casual/casual-2.jpg'
+import img3 from './assets/casual/casual-3.jpg'
+import img4 from './assets/casual/casual-4.jpg'
+import img5 from './assets/casual/casual-5.jpg'
+
+const PageTitle = styled.h1`
+  font-size: 2.5em;
+  text-align: center;
+  color: #000000;
+  margin-bottom: 1em;
+`;
+
+const CarouselContainer = styled(Carousel)`
+  max-width: 600px;
+  max-height: auto;
+  margin: 0 auto;
+`;
+
+const StyledImage = styled.img`
+  width: 600px;
+  height: auto; 
+`;
+
+const Casual = () => {
   return (
-    <>
-      <ListGroup as="ul">
-        <ListGroup.Item as="li" active>
-          Cras justo odio
-        </ListGroup.Item>
-        <ListGroup.Item as="li">Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item as="li" disabled>
-          Morbi leo risus
-        </ListGroup.Item>
-        <ListGroup.Item as="li">Porta ac consectetur ac</ListGroup.Item>
-      </ListGroup>
-      <div className="card_container">
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src=".../sapato-casual01img-1.jpg/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </div>
-
-      <Container fluid>
-        <Row>
-          <Col>1 of 1</Col>
-        </Row>
-      </Container>
-
-      <Container fluid="md">
-        <Row>
-          <Col>1 of 1</Col>
-        </Row>
-      </Container>
-    </>
+    <div>
+      <PageTitle>Casual</PageTitle>
+      <CarouselContainer>
+        <Carousel data-bs-theme="dark">
+          <Carousel.Item>
+            <StyledImage src={img1} alt="Item 1" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <StyledImage src={img2} alt="Item 2" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <StyledImage src={img3} alt="Item 3" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <StyledImage src={img4} alt="Item 4" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <StyledImage src={img5} alt="Item 5" />
+          </Carousel.Item>
+        </Carousel>
+      </CarouselContainer>
+    </div>
   );
 }
+
 export default Casual;
--- src/Feminino.jsx
+++ src/Feminino.jsx
@@ -1,11 +1,60 @@
 import React from 'react'
+import styled from 'styled-components';
+
+// Importando carrossel do bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import Carousel from "react-bootstrap/Carousel";
+
+// Importando imagens
+import img1 from './assets/feminino/feminino-1.jpg'
+import img2 from './assets/feminino/feminino-2.jpg'
+import img3 from './assets/feminino/feminino-3.jpg'
+import img4 from './assets/feminino/feminino-4.jpg'
+import img5 from './assets/feminino/feminino-5.jpg'
+
+const PageTitle = styled.h1`
+  font-size: 2.5em;
+  text-align: center;
+  color: #000000;
+  margin-bottom: 1em;
+`;
+
+const CarouselContainer = styled(Carousel)`
+  max-width: 600px;
+  max-height: auto;
+  margin: 0 auto;
+`;
+
+const StyledImage = styled.img`
+  width: 600px;
+  height: auto; 
+`;
 
 const Feminino = () => {
   return (
     <div>
-      <h1>Produtos femininos</h1>
+      <PageTitle>Feminino</PageTitle>
+      <CarouselContainer>
+        <Carousel data-bs-theme="dark">
+          <Carousel.Item>
+            <StyledImage src={img1} alt="Item 1" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <StyledImage src={img2} alt="Item 2" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <StyledImage src={img3} alt="Item 3" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <StyledImage src={img4} alt="Item 4" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <StyledImage src={img5} alt="Item 5" />
+          </Carousel.Item>
+        </Carousel>
+      </CarouselContainer>
     </div>
-  )
+  );
 }
 
-export default Feminino
+export default Feminino;
